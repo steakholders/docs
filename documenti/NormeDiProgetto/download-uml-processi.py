52b683e826891fcda0a87aaeb19b7de9131195f8
Federico Poli <fedux11@gmail.com>
2013-12-16 21:22:24 +0100

    Tentativo fallito di scaricare le immagini con lo script

--- documenti/NormeDiProgetto/download-uml-processi.py
+++ documenti/NormeDiProgetto/download-uml-processi.py
@@ -2,6 +2,8 @@
 # -*- coding: UTF-8 -*-
 
 import mechanize
+import urllib
+import re
 
 def login (br, username, password):
 	print "Eseguo il login per %s..." % username
@@ -14,7 +16,7 @@ def login (br, username, password):
 	print "Invio i dati per il login..."
 	br.submit().read()
 
-def download_image(br, documentid, destination):
+def download_image(br, userid, documentid, destination):
 	"""
 	Usa le api per le immagini:
 	https://www.lucidchart.com/documents/image/<documentId>/<pageNum>/<width>/<square>
@@ -24,8 +26,33 @@ def download_image(br, documentid, destination):
 	square 	if 1 then return square image, otherwise return an image in the original aspect ratio
 	"""
 	
+	print "Apro il documento %s..." % documentid
+	doc = br.open("https://www.lucidchart.com/documents/view/%s" % documentid).read()
+	pdfdata = ""
+	for line in doc.split('\n'):
+		if "pdfdata" in line:
+			pdfdata = line
+			break
+	
+	print pdfdata
+	
+	pdfdata = re.sub('^.*"pdfdata":', '', pdfdata)
+	pdfdata = re.sub(',"Change":\[\].*$', '', pdfdata)
+	
+	print pdfdata
+	
+	return
+	
+	parameters = {
+		'parameter1' : 'your content',
+		'parameter2' : 'a constant value',
+		'parameter3' : 'unique characters you might need to extract from the page'
+	}
+	#Encode the parameters
+	data = urllib.urlencode(parameters)
+
 	print "Genero l'immagine del documento %s..." % documentid
-	result = br.open("https://www.lucidchart.com/documents/pdf/%s/101012627/png/300" % documentid).read()
+	result = br.open("https://www.lucidchart.com/documents/pdf/%s/%s/png/300" % (documentid, userid), data).read()
 	print result
 	
 	if '"success":false' in result:
@@ -45,5 +72,5 @@ br = mechanize.Browser()
 username = "federico.poli.1@studenti.unipd.it"
 password = raw_input('Inserisci la password per %s:' % username)
 login(br, username, password)
-download_image(br, "471f-1654-52ae2a57-ad48-71840a005f9d", "uml-processi/img.png")
+download_image(br, "101012627", "471f-1654-52ae2a57-ad48-71840a005f9d", "uml-processi/img.png")
 

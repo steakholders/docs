d84b3a7d2c86f4ed9048143bc93f434cf4d8f160
Federico Poli <fedux11@gmail.com>
2014-01-07 23:28:09 +0100

    Abilitato lo script di download da LucidChart

--- documenti/NormeDiProgetto/download-uml-processi.py
+++ documenti/NormeDiProgetto/download-uml-processi.py
@@ -122,5 +122,12 @@ else:
 	callback_url = sys.argv[1]
 
 lc = LucidchartClient(callback_url = callback_url)
-lc.download_image("471f-1654-52ae2a57-ad48-71840a005f9d", "uml-processi/img.png")
+
+
+lc.download_image("4f51-1824-52ae25da-a646-74fd0a00d457", "uml-processi/Pianificazione_compito_e_verifica.png")
+lc.download_image("471f-1654-52ae2a57-ad48-71840a005f9d", "uml-processi/Richiesta_di_modifica_e_segnalazione_bug.png")
+lc.download_image("4aa8-02e4-52ae2830-9412-5cb50a0086f4", "uml-processi/Valutazione_delle_modifiche.png")
+lc.download_image("4557-ff54-52ae279d-8ccc-58080a00c462", "uml-processi/Esecuzione_verifica.png")
+lc.download_image("45ee-6c9c-52ae271d-88b0-07070a0086f4", "uml-processi/Esecuzione_compito.png")
+lc.download_image("4901-7cc4-52ae1b50-a03c-42480a0086f4", "uml-processi/Creazione_compito.png")
 

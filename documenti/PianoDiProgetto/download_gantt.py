b7997a928b0d9e0a57c63dff51d7b7cd50b61922
Federico Poli <fedux11@gmail.com>
2014-01-12 17:20:19 +0100

    Modificato lo script gantt

--- documenti/PianoDiProgetto/download_gantt.py
+++ documenti/PianoDiProgetto/download_gantt.py
@@ -13,7 +13,7 @@ MIN_HOURS = 95
 MAX_HOURS = 105
 # Minimo e massimo di ore di lavoro al giorno per mersona
 MIN_HOURS_PER_DAY = 1
-MAX_HOURS_PER_DAY = 4
+MAX_HOURS_PER_DAY = 3
 # Numero minimo di ruoli che una persona deve svolgere, ciascuno per almeno tot ore
 MIN_ROLES_PER_PERSON = 6
 MIN_HOURS_PER_PERSON_ROLE = 2
